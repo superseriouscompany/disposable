bca819dda248a84bd1db3f16ce3fa61713dbf5f5
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-06-25 15:35:27 +0300

    Animate banner down

--- components/Banner.js
+++ components/Banner.js
@@ -3,33 +3,49 @@
 import React, {Component} from 'react'
 import {connect}          from 'react-redux';
 import {
+  Animated,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native'
 
+const HEIGHT = 40
+
 class Banner extends Component {
   constructor(props) {
     super(props)
-    this.state   = {}
+    this.state = {
+      anim: new Animated.Value(0)
+    }
     this.dismiss = this.dismiss.bind(this)
   }
 
   dismiss() {
-    this.setState({
-      hidden: true,
-    })
+    Animated.timing(this.state.anim, {
+      toValue: 0, duration: 250,
+    }).start()
   }
 
   componentDidMount() {
+    Animated.timing(this.state.anim, {
+      toValue: 1, duration: 250,
+    }).start()
+
     setTimeout(this.dismiss, 3000)
   }
 
   render() {
     const {props} = this
     return this.state.hidden || !props.text ? null : (
-      <TouchableOpacity style={style.banner} onPress={this.dismiss}>
+      <TouchableOpacity style={[style.banner, {
+        transform: [{
+          translateY: this.state.anim.interpolate({
+            inputRange:  [0, 1],
+            outputRange: [-HEIGHT, 0],
+          })
+        }]
+      }]} onPress={this.dismiss}>
         <Text style={style.text}>{props.text}</Text>
       </TouchableOpacity>
     )
@@ -37,9 +53,11 @@ class Banner extends Component {
 }
 
 function mapStateToProps(state) {
-  const failedPhotos = state.outbox.queue.filter((i) => {
+  var failedPhotos = state.outbox.queue.filter((i) => {
     return !!i.failureCount
   }).length
+
+  failedPhotos = 5
 
   return {
     text: failedPhotos ? `Waiting for internet to upload ${failedPhotos} photos.` : null,
@@ -57,7 +75,7 @@ const style = StyleSheet.create({
     position: 'absolute',
     top: 0,
     width: '100%',
-    height: 40,
+    height: HEIGHT,
     backgroundColor: '#999',
     justifyContent: 'center',
     alignItems: 'center',

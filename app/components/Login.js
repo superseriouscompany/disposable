e187a2a4b7c90517bab36d3a540b315859736d05
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-06-26 11:51:57 +0300

    Basic styling of login scene

--- app/components/Login.js
+++ app/components/Login.js
@@ -3,7 +3,9 @@
 import React, {Component} from 'react'
 import {connect}          from 'react-redux';
 import {
+  StyleSheet,
   Text,
+  TextInput,
   TouchableOpacity,
   View,
 } from 'react-native'
@@ -20,11 +22,59 @@ class Login extends Component {
   }
 
   render() { return (
-    <TouchableOpacity onPress={this.login}>
-      <Text>Hello</Text>
-    </TouchableOpacity>
+    <View style={style.container}>
+      <View style={style.fields}>
+        <TextInput
+          placeholder={'Name'}
+          style={[style.input]}
+          />
+        <TextInput
+          placeholder={'Email'}
+          style={[style.input]}
+          />
+      </View>
+      <TouchableOpacity style={style.button} onPress={this.login}>
+        <Text style={style.buttonText}>Enter</Text>
+      </TouchableOpacity>
+    </View>
   )}
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  fields: {
+    // flex: 1,
+    // justifyContent: 'center',
+    paddingLeft: '20%',
+    paddingRight: '20%',
+  },
+
+  button: {
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    padding: 5,
+    borderRadius: 5,
+    backgroundColor: '#ffc50c',
+    overflow: 'hidden',
+    color: 'white',
+    fontSize: 20,
+  },
+
+  input: {
+    height: 25,
+    padding: 5,
+    borderColor: '#c9c9c9',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 25,
+  }
+})
 
 function mapStateToProps(state) {
   return {
